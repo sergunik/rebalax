652e028dcc108a9d72b93419e8bd9c2c1e819e76
Sergunik <sergunik@gmail.com>
2025-08-02 22:22:21 +0400

    Add Grafana database configuration to environment files and update deployment scripts

--- docker/grafana/entrypoint.sh
+++ docker/grafana/entrypoint.sh
@@ -2,9 +2,9 @@
 
 cp /etc/grafana/provisioning/datasources/datasource.yml.example /etc/grafana/provisioning/datasources/datasource.yml
 
-sed -i "s|\${{ env.DB_USERNAME }}|${DB_USERNAME}|g" /etc/grafana/provisioning/datasources/datasource.yml
-sed -i "s|\${{ env.DB_PASSWORD }}|${DB_PASSWORD}|g" /etc/grafana/provisioning/datasources/datasource.yml
-sed -i "s|\${{ env.DB_DATABASE }}|${DB_DATABASE}|g" /etc/grafana/provisioning/datasources/datasource.yml
+sed -i "s|\$env.GRAFANA_DB_USERNAME|${GRAFANA_DB_USERNAME}|g" /etc/grafana/provisioning/datasources/datasource.yml
+sed -i "s|\$env.GRAFANA_DB_PASSWORD|${GRAFANA_DB_PASSWORD}|g" /etc/grafana/provisioning/datasources/datasource.yml
+sed -i "s|\$env.GRAFANA_DB_DATABASE|${GRAFANA_DB_DATABASE}|g" /etc/grafana/provisioning/datasources/datasource.yml
 
 
 exec /run.sh
