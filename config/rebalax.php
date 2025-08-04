2363a4d89540c53a116ca04d4c7c5ee8909297eb
Sergunik <sergunik@gmail.com>
2025-08-04 22:11:09 +0400

    Refactor scheduling commands and update service names in Docker configuration

--- config/rebalax.php
+++ config/rebalax.php
@@ -26,8 +26,8 @@ return [
         'simple' => [
             'enabled' => true,
             'threshold_percent' => 7.0,
-            'batch_size' => 1000,
-            'timeout' => 22, // seconds
+            'batch_size' => 500,
+            'timeout' => 25, // seconds
         ],
     ],
 ];
