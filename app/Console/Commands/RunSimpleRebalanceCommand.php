7e71fddf1ae732932fe21818e6e612d923cfb7d0
Sergunik <sergunik@gmail.com>
2025-08-10 01:29:43 +0400

    Improved rebalance command

--- app/Console/Commands/RunSimpleRebalanceCommand.php
+++ app/Console/Commands/RunSimpleRebalanceCommand.php
@@ -4,24 +4,25 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Models\Portfolio;
 use App\Services\MetricsService;
 use App\Services\SimpleRebalanceService;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Cache\Repository;
 use Throwable;
 
 class RunSimpleRebalanceCommand extends Command
 {
     private const COMMAND_NAME = 'rebalance_simple';
 
-    protected $signature = 'app:rebalance:simple
-        {--batchOffset=0 : Offset for batch processing}
-        {--batchSize=100 : Number of portfolios to process in a single batch}';
+    protected $signature = 'app:rebalance:simple';
 
     protected $description = 'Run a simple rebalance of the portfolio based on predefined rules';
 
     public function __construct(
         private readonly SimpleRebalanceService $rebalanceService,
-        private readonly MetricsService $metricsService
+        private readonly Repository $cacheRepository,
+        private readonly MetricsService $metricsService,
     ) {
         parent::__construct();
     }
@@ -31,29 +32,36 @@ class RunSimpleRebalanceCommand extends Command
         $timeStart = microtime(true);
         $status = 'success';
 
-        // Mark command as active
         $this->metricsService->setActiveCommand(self::COMMAND_NAME);
 
-        try {
-            $batchOffset = (int) $this->option('batchOffset');
-            $batchSize = $this->hasOption('batchSize')
-                ? (int) $this->option('batchSize')
-                : config('rebalax.rebalance.simple.batch_size');
-            $totalCount = 0;
+        $totalPortfolios = $this->getCachedTotalPortfoliosCount();
 
-            while (true) {
+        if ($totalPortfolios === 0) {
+            $this->info('No portfolios to process.');
+            return 0;
+        }
+
+        $runsPerHour = 12;
+
+        $currentMinute = (int) date('i');
+        $currentBatchIndex = intdiv($currentMinute, 5);
+
+        $globalBatchSize = (int) ceil($totalPortfolios / $runsPerHour);
+        $globalBatchOffset = $globalBatchSize * $currentBatchIndex;
+        $countOfIterations = (int) ceil($globalBatchSize / config('rebalax.rebalance.simple.batch_size'));
+        $localBatchSize = min($globalBatchSize, config('rebalax.rebalance.simple.batch_size'));
+
+        try {
+            $totalCount = 0;
+            for ($i = 0; $i < $countOfIterations; $i++) {
+                $batchOffset = $globalBatchOffset + ($i * $localBatchSize);
+                $batchSize = min($localBatchSize, $globalBatchSize * ($currentBatchIndex+1) - $batchOffset);
                 $count = $this->rebalanceService->do($batchOffset, $batchSize);
 
                 // Record batch metrics
                 $this->metricsService->recordBatchProcessed(self::COMMAND_NAME, $batchSize, $count);
 
                 $totalCount += $count;
-
-                if ($count) {
-                    $batchOffset += $batchSize;
-                } else {
-                    break;
-                }
 
                 if (microtime(true) - $timeStart > config('rebalax.rebalance.simple.timeout')) {
                     $this->metricsService->recordCommandTimeout(self::COMMAND_NAME);
@@ -70,14 +78,22 @@ class RunSimpleRebalanceCommand extends Command
         } finally {
             $executionTime = microtime(true) - $timeStart;
 
-            // Record metrics
             $this->metricsService->recordCommandExecutionTime(self::COMMAND_NAME, $executionTime);
             $this->metricsService->incrementCommandExecutions(self::COMMAND_NAME, $status);
-
-            // Mark command as inactive
             $this->metricsService->setActiveCommand(self::COMMAND_NAME, 0);
         }
 
         return $status === 'success' ? 0 : 1;
+    }
+
+    private function getCachedTotalPortfoliosCount(): int
+    {
+        return (int) $this->cacheRepository->remember(
+            'rebalance_total_portfolios_count',
+            now()->addDay(),
+            function () {
+                return Portfolio::count();
+            }
+        );
     }
 }
